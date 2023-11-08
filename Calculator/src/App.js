08da94b63229a0cc86bc79ce5a3c2023124bb110
Rodrigo <144171797+Rodrigo5431@users.noreply.github.com>
2023-11-08 23:08:15 +0000

    grande progresso

--- Calculator/src/App.js
+++ Calculator/src/App.js
@@ -4,53 +4,73 @@ import Opcao from './components/opcao/Opcao';
 
 function App() {
     const [valores, setValores] = useState ([
-      {"valor": "vazio"},
-      {"valor": "player1"},
-      {"valor": "player2"},
-      {"valor": "vazio"},
-      {"valor": "vazio"},
-      {"valor": "vazio"},
-      {"valor": "vazio"},
-      {"valor": "vazio"},
-      {"valor": "vazio"}
+        {"valor": "vazio"},
+        {"valor": "vazio"},
+        {"valor": "vazio"},
+        {"valor": "vazio"},
+        {"valor": "vazio"},
+        {"valor": "vazio"},
+        {"valor": "vazio"},
+        {"valor": "vazio"},
+        {"valor": "vazio"}
     ]);
-   const [jogador, setJogador] = useState (1);
+    const [jogador, setJogador] = useState (1);
 
-    alert(`Vez do jogador ${jogador}`)
+    // alert(`Vez do jogador ${jogador}`)
 
-   function aoClicar(index){
-    if(jogador === 1){
-     setJogador (2);
-     //setValores
-     // valores[index] = 1
-     console.log(jogador)
+    // function verificaVitoria(valores){
+    //   const linhas = [
+    //     [0, 1, 2], [3, 4, 5], [6, 7, 8],  
+    //     [0, 3, 6], [1, 4, 7], [2, 5, 8],  
+    //     [0, 4, 8], [2, 4, 6]]
+
+    //     if( valores == linhas){
+    //       console.log("voce ganhou")
+    //     }
+      
+      //alert parabens fulano
+      //declara uma variavel vazio
+      //setValores(variavelVazia)
     }
-    else if(jogador === 2){
-      setJogador(1);
-      console.log(jogador)
-      // valores[index] = 2
+
+    function aoClicar(index){
+        if(valores[index].valor === "player1" || valores[index].valor === "player2"){
+          alert("Nao pode");
+        } else {
+          if(jogador == 1){
+            let temp = [...valores];
+            temp[index].valor = "player1";
+            setValores(temp);
+            setJogador(2);
+            console.log(`Vez do jogador ${jogador} (1)`);
+          }else if(jogador == 2){
+            setJogador(1);
+            let temp = [...valores];
+            temp[index].valor = "player2";
+            setValores(temp);
+            console.log(`Vez do jogador ${jogador} (2)`);
+          }
+        }
+        verificaVitoria();
     }
 
+    return (
+        <body>
 
-   }
+            <section className="container">
+                <h1>JOGO DA VELHA</h1>
+            </section>
 
-  return (
-    <body>
+            <section className='tabuleiro'>
+                {valores.map( (item, index) => {
+                return (
+                  <Opcao aoClicar={aoClicar} item={item} index={index} />
+                );
+                })}
+            </section>
 
-      <section className="container">
-          <h1>JOGO DA VELHA</h1>
-      </section>
-
-      <section className='tabuleiro'>
-          {valores.map( (item, index) => {
-            return (
-              <Opcao  aoClicar={aoClicar} item={item} index={index} />
-            );
-          })}
-      </section>
-
-    </body>
-  );
+        </body>
+    );
 }
 
 export default App;
--- Calculator/src/components/opcao/Opcao.jsx
+++ Calculator/src/components/opcao/Opcao.jsx
@@ -2,7 +2,7 @@ import './style.css'
 
 export default function Opcao (props){
     return(
-        <section onClick={props.aoClicar(props.index)} id={props.index} className={props.item.valor} />
+        <section onClick={() => props.aoClicar(props.index)} id={props.index} className={props.item.valor} />
     );
 }   
   
